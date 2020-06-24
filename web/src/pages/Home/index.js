5bd18c1294aa497fa4b6623cbea5e050c094f9b1
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-24 20:40:28 -0300

    Home page

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import logo from '../../assets/logo.png';
 
-import { Container, Header } from './styles';
+import { Header, Content } from './styles';
 
 function Home() {
   return (
-    <Container>
+    <>
       <Header>
         <div className="logo">
           <img src={logo} alt="logo"/>
@@ -19,7 +19,19 @@ function Home() {
           </button>
         </div>
       </Header>
-    </Container>
+      <Content>
+        <div className="text">
+          <h1>Faça mais com o DuolcCloud</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum modi mollitia necessitatibus velit ipsum, placeat nulla sit? Laboriosam quam id facilis</p>
+          <button type="button">
+            Crie uma conta
+          </button>
+        </div>
+        <div className="image">
+          <img src="https://lh3.googleusercontent.com/ECyrzOoBiRQiYuSHIEFLet_HGwgbo32CoN-ltRJSwWJAAmhqOolGQEiZCxRmdWLeRoaHxBO3PDSTL98mfR3h5cVTj8N8yqqUr3lq=w790" alt="gmail"/>
+        </div>
+      </Content>
+    </>
   );
 }
 
--- web/src/pages/Home/styles.js
+++ web/src/pages/Home/styles.js
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  
-`;
 
 export const Header = styled.header`
   /* margin: 8px 8px; */
@@ -10,6 +7,9 @@ export const Header = styled.header`
   justify-content: space-between;
   align-items: center;
   box-shadow: 2px 2px 8px 1px rgba(0, 0, 0, 0.2);
+  /* z-index: unset; */
+  
+  height: 10%;
 
 
   .logo {
@@ -34,7 +34,7 @@ export const Header = styled.header`
   }
   .right a {
     color: rgba(0,0,0,0.65);
-    margin-right: 16px;
+    margin-right: 24px;
   }
 
   .right button {
@@ -46,8 +46,48 @@ export const Header = styled.header`
     letter-spacing: 0.2px;
   }
 
+  .right button:hover {
+    opacity: 0.9;
+    transition: 200ms;
+  }
+
 `;
 
 export const Content = styled.div`
-  
+  display: flex;
+  padding: 0px 80px;
+  align-items: center;
+  color: rgba(0,0,0,0.8);
+  background-color: #F8F9FA;
+  height: 90%;
+
+  button {
+    padding: 12px 22px;
+    border: 0;
+    background-color: #1A73E8;
+    color: white;
+    font-size: 17px;
+    letter-spacing: 0.2px;
+  }
+
+  button:hover {
+    opacity: 0.9;
+    transition: 200ms;
+  }
+
+  .text h1 {
+    font-size: 48px;
+    margin-bottom: 20px;
+  }
+
+  .text p {
+    margin-bottom: 30px;
+    font-size: 16px;
+  }
+
+  .image img {
+    /* min-width: 300px; */
+    width: 500px;
+  }
+
 `;
