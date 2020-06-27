30eba503c09baa6f03a65bd760bf3e7ca4a424b0
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-26 22:54:59 -0300

    user and file migration

---
+++ backend/src/database/migrations/20200627015154-create-files.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id :{
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('files');
+  },
+};
